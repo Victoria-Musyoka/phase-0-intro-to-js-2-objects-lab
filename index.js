073cd3d48feffa335bb44b24aed6c67806f479c8
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-03 10:41:25 +0300

    completing assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Victoria",
+    streetAddress:"482826"
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){//parameter}
+    const newObject ={...employee}
+    newObject[key] = value;
+    return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee,key){
+    const delObject= {...employee};
+    delete delObject[key]
+    return delObject
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee
+}
